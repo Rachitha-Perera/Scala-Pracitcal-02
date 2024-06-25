b1f97ecc007bd51a030575402975a226b9757ef1
Rachitha-Perera <2022cs154@stu.ucsc.cmb.ac.lk>
2024-06-25 09:18:57 +0530

    Add files via upload

---
+++ P2/Scala-Pracitcal-02/P2/Q1.scala
@@ -0,0 +1,17 @@
+object variables{
+    def main(args:Array[String]):Unit={
+        var k,i,j=2
+        var m,n=5
+        var f=12.0f
+        var g=4.0f
+        var c="X"
+        var a1=k+12*m
+        var a2=m/j
+        var a3=n%j
+        var a4=m/j*j
+        var a5=f+10*5+g
+        //var a6= (++i)*n;
+
+        printf("answer 1="+a1+"\nanswer 2="+a2+"\nanswer 3="+a3+"\nanswer 4="+a4+"\nanswer 5="+a5+"\nanswer 6="+" No \'++\' operator in scala\n")
+    }
+}
